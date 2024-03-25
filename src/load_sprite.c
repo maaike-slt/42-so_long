e3cc8241141da888f3cf170f32e84f0a688e0650
maaike-slt <maaikesloot@home.nl>
2024-03-25 22:55:04 +0100

    revert

--- src/load_sprite.c
+++ src/load_sprite.c
@@ -6,7 +6,7 @@
 /*   By: msloot <msloot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/10 14:17:48 by msloot            #+#    #+#             */
-/*   Updated: 2024/03/25 21:09:03 by msloot           ###   ########.fr       */
+/*   Updated: 2024/03/25 22:54:04 by msloot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,32 +24,6 @@ static bool	load_sprite_err(const char *path)
 
 #include <stdio.h>
 
-static void	apply_transparency(t_img *img)
-{
-	int		bits_per_pixel;
-	int		size_line;
-	int		endian;
-	char	*buffer;
-
-	buffer = mlx_get_data_addr(img->ptr, &bits_per_pixel, &size_line, &endian);
-	printf("pixel: %x\n", *(int *)(buffer));
-	for (int y = 0; y < img->h; ++y)
-	{
-    	for (int x = 0; x < img->w; ++x)
-    	{
-        	int pixel = (y * size_line) + (x * 4);
-        	unsigned int color = *(int *)(buffer + pixel);
-
-        	// Check if the pixel is black
-        	if (color == 0xff000000)
-        	{
-            	// Change to transparent (assuming 0x00000000 for transparent)
-            	*(int *)(buffer + pixel) = 0x00FF0000;
-        	}
-    	}
-	}
-}
-
 static bool	load_sprite(t_env *env, t_img *img, char *xpm_path)
 {
 	img->w = 0;
@@ -61,7 +35,6 @@ static bool	load_sprite(t_env *env, t_img *img, char *xpm_path)
 			&(img->h));
 	if (!img->ptr)
 		return (load_sprite_err(xpm_path));
-	apply_transparency(img);
 	printf("%s\nw: %d\nh: %d\n\n", xpm_path, img->w, img->h);
 	return (true);
 }
