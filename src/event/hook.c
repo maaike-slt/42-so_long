ea6c5de33e6e71045603ab5ade12f9aeeb864460
maaike-slt <maaikesloot@home.nl>
2024-03-26 21:21:21 +0100

    keycode

--- src/event/hook.c
+++ src/event/hook.c
@@ -6,7 +6,7 @@
 /*   By: msloot <msloot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 19:43:14 by msloot            #+#    #+#             */
-/*   Updated: 2024/03/26 14:41:44 by msloot           ###   ########.fr       */
+/*   Updated: 2024/03/26 21:01:27 by msloot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 void	set_hook(t_env *env)
 {
-//	mlx_key_hook(env->win.ptr, move_pony, env);
+	mlx_key_hook(env->win.ptr, handle_keycode, env);
 //	mlx_loop_hook(env->mlx, move_pony, env);
-//	mlx_loop(env->mlx);
+	// mlx_loop(env->mlx);
 	mlx_hook(env->win.ptr, ON_DESTROY, NO_EVENT, close_win, env);
 }
--- src/event/move_pony.c
+++ src/event/move_pony.c
@@ -6,18 +6,47 @@
 /*   By: msloot <msloot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/26 13:26:25 by msloot            #+#    #+#             */
-/*   Updated: 2024/03/26 14:42:16 by msloot           ###   ########.fr       */
+/*   Updated: 2024/03/26 21:20:53 by msloot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 #include <stdio.h>
 
-int	move_pony(int keycode, t_env *env)
+int	handle_keycode(int keycode, t_env *env)
 {
-	(void)keycode;
+	// (void)keycode;
 	(void)env;
-
-	printf("Hello from key_hook!\n");
+	if (movement(keycode, env))
+		return (0);
+	else
+	{
+		ft_putstr("the keycode ");
+		ft_putstr(Y_B_MAG);
+		ft_putnbr(keycode);
+		ft_putstr(Y_RESET);
+		ft_putstr(" does not have any effect\n");
+	}
 	return (0);
+}
+
+bool	movement(int keycode, t_env *env)
+{
+	if (keycode == K_UP || keycode == K_W)
+		return (move_pony(env, 0, -1));
+	else if (keycode == K_DOWN || keycode == K_S)
+		printf("key: %d\tdown\n", keycode);
+	else if (keycode == K_LEFT || keycode == K_A)
+		printf("key: %d\tleft\n", keycode);
+	else if (keycode == K_RIGHT || keycode == K_D)
+		printf("key: %d\tright\n", keycode);
+	else
+		return (false);
+}
+
+bool	move_pony(t_env *env, ssize_t x, ssize_t y)
+{
+//	env.
+	printf("%s\n", "key: up");
+	return (true);
 }
